076db08fbd40380b73e7f156012ac13337ec0afa
= <=>
2025-11-05 18:32:12 +0530

    ex8 host fix

--- ex9/server/server.js
+++ ex9/server/server.js
@@ -27,6 +27,7 @@ app.use("/api/quizzes", quizRoutes);
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
+const HOST = "0.0.0.0";
 
 app.get("/", (req, res) => res.send("Backend Running"));
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, HOST, () => console.log(`Server running on port ${PORT}`));
